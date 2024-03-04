0881cb349837ca84dfe8b6730f15618f03c85e49
jessicareinh <148791371+jessicareinh@users.noreply.github.com>
2024-03-04 19:28:59 +0100

    initial commit

--- src/components/FavoriteButton/index.js
+++ src/components/FavoriteButton/index.js
@@ -1,10 +1,11 @@
 import "./FavoriteButton.css";
+import { useState } from "react";
 import { ReactComponent as StarFilled } from "./star-filled.svg";
 import { ReactComponent as Star } from "./star.svg";
 
 export default function FavoriteButton() {
   // this should be a state variable
-  const isFavorite = false;
+  const [isFavorite, setIsFavorite] = useState(false);
 
   return (
     <button
